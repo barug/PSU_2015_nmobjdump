656ed377895132986f0485e1253ab0fc6d4d5fd4
Barthélemy Gouby <barthelemy.gouby@gmail.com>
2016-02-18 12:09:56 +0100

    initial commit nm

---
+++ Makefile
@@ -0,0 +1,44 @@
+##
+## Makefile for PSU_2015_nmobjdump in /home/barthe_g/rendu/tek2/unix_memoire/PSU_2015_nmobjdump
+## 
+## Made by Barthelemy Gouby
+## Login   <barthe_g@epitech.net>
+## 
+## Started on  Tue Feb 16 09:43:25 2016 Barthelemy Gouby
+## Last update Wed Feb 17 14:50:49 2016 Barthelemy Gouby
+##
+
+CC	= gcc
+
+RM	= rm -f 
+
+NAME1	= my_nm
+
+NAME2	= my_objdump
+
+SRCS1	= main_nm.c
+
+SRCS2	= main_obdump.c
+
+OBJS1	= $(SRCS1:.c=.o)
+
+OBJS2	= $(SRCS2:.c=.o)
+
+CFLAGS 	= -Wall -Werror -Wextra
+
+nm: $(OBJS1)
+	$(CC) -o $(NAME1) $(OBJS1) $(CFLAGS)
+
+objdump: $(OBJS2)
+	$(CC) -o $(NAME2) $(OBJS2) $(CFLAGS)
+
+all: nm objdump
+
+clean: $(RM) $(OBJS1) $(OBJS2)
+
+fclean: clean
+	$(RM) $(NAME1) $(NAME2)
+
+re: fclean all
+
+.PHONY: nm objdump all clean fclean re
---
+++ nm_ressources.h
@@ -0,0 +1,46 @@
+//
+// nm_ressources.h for nm in /home/barthe_g/rendu/tek2/unix_memoire/PSU_2015_nmobjdump
+// 
+// Made by Barthelemy Gouby
+// Login   <barthe_g@epitech.net>
+// 
+// Started on  Tue Feb 16 11:01:19 2016 Barthelemy Gouby
+// Last update Tue Feb 16 19:49:35 2016 Barthelemy Gouby
+//
+
+#ifndef _MY_NM
+# define _MY_NM
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <string.h>
+#include <elf.h>
+#include <ctype.h>
+
+typedef struct	s_arguments
+{
+  char		**file_paths;
+}		t_arguments;
+
+typedef struct	s_sym_info
+{
+  char		*symbol_names;
+  Elf64_Sym	*symbol_table;
+  size_t	st_entry_size;
+  size_t	st_size;
+  size_t	st_length;
+}		t_sym_info;
+
+typedef struct	s_sym
+{
+  char		*name;
+  uint64_t	value;
+}		t_sym;
+
+#endif /* !_MY_NM */
